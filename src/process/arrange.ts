9163d2929157172577e0331121528190a8aa3401
raffimolero <raffimolero@gmail.com>
2025-01-10 00:36:33 +0800

    split arrange into subjects and schedule

--- src/process/arrange.ts
+++ src/process/arrange.ts
@@ -48,16 +48,25 @@ function get_times(data: SisData): TimeMinutes[] {
 // TODO: remove unnecessary weekdays
 // TODO: delete table tiles that will be overwritten by rowspan and colspan
 
-export function arrange(data: SisData): { subjects: SubjectData[], schedule: ScheduleTable } {
-    const subjects: SubjectData[] = []
+function arrange_subjects(data: SisData): SubjectData[] {
+    return []
+}
+
+function arrange_schedule(data: SisData): ScheduleTable {
     const table: ScheduleRow[] = []
+    
+    // get all the unique timestamps in the data and sort them
     const minutes = get_times(data)
+
+    // no timestamps means no data
     if (minutes.length === 0) {
-        return { subjects, schedule: { table } }
+        return { table }
     }
-    console.log(minutes)
-    let prev_minutes = minutes[0]
+
+    // fill the table with empty cells
+    // compute the size of each row by the number of minutes between each timestamp
     minutes.push(minutes[minutes.length - 1] + 30)
+    let prev_minutes = minutes[0]
     for (const time of minutes.slice(1)) {
         table.push({
             time: minutes_time(prev_minutes),
@@ -66,16 +75,17 @@ export function arrange(data: SisData): { subjects: SubjectData[], schedule: Sch
         })
         prev_minutes = time
     }
-    // console.log(table)
-    
+
+    // fill in the table data
     for (const subject of data.subjects) {
         for (const subj_slot of subject.subj_sched) {
             const start_row = minutes.indexOf(time_minutes(subj_slot.start))
             const end_row = minutes.indexOf(time_minutes(subj_slot.end))
             const rowspan = end_row - start_row
-            console.log(subj_slot.start, start_row, subj_slot.end, end_row)
+
             for (const weekday of subj_slot.weekdays) {
                 const col = weekday_index(weekday)
+                // add the main subject tile
                 table[start_row].columns[col] = {
                     data: {
                         type: 'subject',
@@ -84,11 +94,25 @@ export function arrange(data: SisData): { subjects: SubjectData[], schedule: Sch
                     colspan: 1,
                     rowspan,
                 }
+                // delete the tiles that the subject overlaps
                 for (let row = start_row + 1; row < end_row; row++) {
                     table[row].columns[col] = null
                 }
             }
         }
     }
-    return { subjects, schedule: { table } }
+    // TODO: post-process the table to detect lunch break
+    // what do you mean saturday's lunch break is different from the weekdays'
+    return { table }
+}
+
+/**
+ * Arranges SIS data into a table close to how HTML would format it.
+ * @param data the SIS data to arrange
+ * @returns the subjects and the schedule
+ */
+export function arrange(data: SisData): { subjects: SubjectData[], schedule: ScheduleTable } {
+    return {
+        subjects: arrange_subjects(data),
+        schedule: arrange_schedule(data), }
 }

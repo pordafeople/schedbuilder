9c42e5cc4f9a3769ee175a90919774af140b208e
raffimolero <raffimolero@gmail.com>
2025-01-10 04:54:31 +0800

    test meaningful change

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>SchedBuilder</h1>
         <InputBox text={text} setText={setText} />
         <br />
         <Schedule {...schedule} />

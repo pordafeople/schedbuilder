0ee121d50582ccaad14b30037a4f1d9806d4ba84
raffimolero <raffimolero@gmail.com>
2025-01-10 14:29:57 +0800

    arrange ClassList from SisData

--- src/render/Classes.tsx
+++ src/render/Classes.tsx
@@ -1,5 +1,18 @@
-function Classes() {
-  return <div></div>
+import { ClassDisplayData, ClassList } from '../process/arrange'
+import './Classes.css'
+
+function ClassDisplay({ class_data, color }: ClassDisplayData, index: number) {
+  return (
+    <tr key={index} className="class" style={{ backgroundColor: color }}>
+      <td>{class_data.code}</td>
+    </tr>
+  )
 }
 
-export default Classes
+export default function Classes({ classes }: { classes: ClassList }) {
+  return (
+    <table className="class-table">
+      <tbody>{classes.map(ClassDisplay)}</tbody>
+    </table>
+  )
+}

4dee1b38fa76537e7ce989575c5b816633a76c6e
Kurev <153285455+Kurev@users.noreply.github.com>
2025-01-11 12:53:55 +0800

    Render subjects

--- src/render/Classes.tsx
+++ src/render/Classes.tsx
@@ -4,7 +4,9 @@ import './Classes.css'
 function ClassDisplay({ class_data, color }: ClassDisplayData, index: number) {
   return (
     <tr key={index} className="class" style={{ backgroundColor: color }}>
-      <td>{class_data.code}</td>
+      <td>{class_data.subject}</td>
+      <td>{class_data.title}</td>
+      <td>{class_data.teacher.given_name}<br /><label>{class_data.teacher.emails}</label></td>
     </tr>
   )
 }

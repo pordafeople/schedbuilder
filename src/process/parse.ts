49a89ae6f8729b6394811a560c9d8508cc237b81
Raffi Molero <raffimolero@gmail.com>
2025-01-15 12:21:47 +0800

    fix multi-word subject codes

--- src/process/parse.ts
+++ src/process/parse.ts
@@ -123,7 +123,7 @@ export type ClassData = {
 }
 
 const class_regex =
-    /(\d+-\d+)\s*(\w+ \d+)\s*([^*]+)\s*\* ((?:.+? \*)+)\s*([^\n]+)?\s*([^ \n]+)?\s*(\d+)\/(\d+)\s*/g
+    /(\d+-\d+)\s*((?:\w+ )+\d+)\s*([^*]+)\s*\* ((?:.+? \*)+)\s*([^\n]+)?\s*([^ \n]+)?\s*(\d+)\/(\d+)\s*/g
 function parse_class(match: RegExpExecArray): ClassData {
     return {
         code: match[1],
